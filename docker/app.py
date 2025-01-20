d31b961bfed6044dd2c9e8843bce028c64483f5e
HeyBlue24 <159277800+HeyBlue24@users.noreply.github.com>
2025-01-20 17:52:16 +0530

    Update app.py

--- docker/app.py
+++ docker/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "V666666666 is working"
+    return "Helloooooooooooooooooooooooooooooooo"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
